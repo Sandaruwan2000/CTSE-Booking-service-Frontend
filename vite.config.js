8501ae7e681439d2385567b629934863bdf44eea
bashitha-weerapperuma <beweerapperuma@gmail.com>
2026-03-22 17:25:24 +0530

    Save all changes to new branch

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
       port: 5173,
       proxy: {
         '/api': {
-          target: env.VITE_DEV_PROXY_TARGET || 'http://localhost:8080',
+          target: env.VITE_DEV_PROXY_TARGET || 'http://localhost:9000',
           changeOrigin: true,
         },
       },
